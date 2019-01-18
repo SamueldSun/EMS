0b1e1ea5e38e3db62fe463f53ec54967558d32fd
Samuel Sun <samueldsun@hotmail.com>
2019-01-18 00:13:20 -0500

    Add files via upload

---
+++ EmployeeDatabaseV4.0/src/employeedatabasev4/pkg0/FullTimeEmployee.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employeedatabasev4.pkg0;
+
+/**
+ *
+ * @author Samuel
+ */
+public class FullTimeEmployee extends EmployeeInfo {
+    
+    private double yearlySalary;
+    
+    public FullTimeEmployee(String fN, String lN, String yB, String mB, String dB, int eN, String wL, String d, double dR, double yS) {
+        
+        super(fN, lN, yB, mB, dB, eN, wL, d, dR);
+        yearlySalary = yS;
+        
+    }
+    
+    
+    @Override
+    public double getAnnualGrossIncome() {
+        return yearlySalary;
+    }
+    
+    public double getYearlySalary(){
+        return yearlySalary;
+    }
+    public void setYearlySalary(double yearlySalary){
+        this.yearlySalary = yearlySalary;
+    }
+    
+    
+}
